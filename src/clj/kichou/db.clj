23adb5a468683440513714fe6b48ae76137ed94e
Linerre <leon.errelin@outlook.com>
2025-04-20 16:17:31 +1000

    Update operations

--- src/clj/kichou/db.clj
+++ src/clj/kichou/db.clj
@@ -4,16 +4,13 @@
             [honey.sql :as sql]
             [honey.sql.helpers :refer [select from where join]]))
 
-;; Define the database connection spec
-;; This uses a relative path to the database file
+;; == Databse =======================================
 (def db-spec
   {:dbtype "sqlite"
    :dbname "db/expense.db"})
 
-;; Create a connection pool for better performance
 (def datasource (jdbc/get-datasource db-spec))
 
-;; Helper function to execute queries
 (defn query [sql-map]
   (jdbc/execute! datasource (sql/format sql-map)))
 
@@ -23,59 +20,60 @@
   (query {:select [:*], :from [:Providers]}))
 
 (defn get-provider-by-abn [abn]
-  (first
-   (query
-    (-> (select :*)
-        (from :Providers)
-        (where [:= :abn abn])))))
+  (-> (select :*)
+      (from :Providers)
+      (where [:= :abn abn])
+      (query)
+      (first)))
 
 (defn get-all-expenses []
-  (query
-   (-> (select :*)
-       (from :Expenses))))
+  (-> (select :*)
+      (from :Expenses)
+      (query)))
 
 (defn get-expenses-by-week [week-number]
-  (query
-   (-> (select :*)
-       (from :Expenses)
-       (where [:= :week_number week-number]))))
+  (-> (select :*)
+      (from :Expenses)
+      (where [:= :week_number week-number])
+      (query)))
 
 (defn get-expenses-by-provider [provider-abn]
-  (query
-   (-> (select :*)
-       (from :Expenses)
-       (where [:= :provider_abn provider-abn]))))
+  (-> (select :*)
+      (from :Expenses)
+      (where [:= :provider_abn provider-abn])
+      (query)))
 
 (defn get-expenses-with-provider-details []
-  (query
-   (-> (select [:e.expense_id :id]
-                      [:e.date :date]
-                      [:e.week_number :week]
-                      [:e.product_name :product]
-                      [:e.price :price]
-                      [:e.quantity :quantity]
-                      [:p.full_brand_name :provider_name]
-                      [:p.location :location])
-       (from [:Expenses :e])
-       (join [:Providers :p] [:= :e.provider_abn :p.abn]))))
+  (-> (select [:e.expense_id :id]
+              [:e.date :date]
+              [:e.week_number :week]
+              [:e.product_name :product]
+              [:e.price :price]
+              [:e.quantity :quantity]
+              [:p.full_brand_name :provider_name]
+              [:p.location :location])
+      (from [:Expenses :e])
+      (join [:Providers :p] [:= :e.provider_abn :p.abn])
+      (query)))
 
 ;; == Updates =======================================
 
-(defn add-provider [abn full-brand-name location website]
+(defn add-provider [{:keys [abn full-brand-name location website]}]
   (jsql/insert! datasource :Providers
-               {:abn abn
-                :full_brand_name full-brand-name
-                :location location
-                :website website}))
+                {:abn             abn
+                 :full_brand_name full-brand-name
+                 :location        location
+                 :website         website}))
 
-(defn add-expense [date week-number product-name price quantity provider-abn]
+(defn add-expense
+  [{:keys [date week-number product-name price quantity provider-abn]}]
   (jsql/insert! datasource :Expenses
-               {:date date
-                :week_number week-number
-                :product_name product-name
-                :price price
-                :quantity quantity
-                :provider_abn provider-abn}))
+                {:date         date
+                 :week_number  week-number
+                 :product_name product-name
+                 :price        price
+                 :quantity     quantity
+                 :provider_abn provider-abn}))
 
 (defn update-provider [abn updates]
   (jsql/update! datasource :Providers updates {:abn abn}))
