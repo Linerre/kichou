ecb066667f7b73475cd5908acecd569a9e328441
Linerre <leon.errelin@outlook.com>
2025-07-02 14:43:28 +1000

    Add a simple dev server

--- dev/hotreload.clj
+++ dev/hotreload.clj
@@ -1,12 +1,28 @@
 ;; dev/hotreload.clj
 (ns hotreload
-  (:require [ring.adapter.jetty :refer [run-jetty]]
+  (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [kichou.server :refer [handler]])
   (:gen-class))
 
+(defonce server (atom nil))
+
 (def dev-handler
   (wrap-reload #'handler))
 
+(defn stop-server! []
+  (when @server
+    (.stop @server)
+    (reset! server nil)
+    (println "Server shut down")))
+
+(defn start-server! [port]
+  (when @server
+    (stop-server!))
+  (reset! server (jetty/run-jetty dev-handler {:port port, :join? false}))
+  (println "Server started at port: " port))
+
 (defn -main [& args]
-  (run-jetty dev-handler {:port 9321, :join? false}))
+  (start-server! 9000))
+
+(comment (-main))
--- src/clj/kichou/server.clj
+++ src/clj/kichou/server.clj
@@ -2,30 +2,41 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [hiccup2.core :as h]
-            [shadow.css :refer (css)])
+            [shadow.css :refer [css]])
   (:gen-class))
 
 ;; To use a atom as server, see: https://gist.github.com/plexus/19ef2874d9f0c56e458e78c2e1103f16
-#_(defonce server (atom nil))
-
+(defonce server (atom nil))
 
 ;; home page for server and dev
 (defn home []
   (-> [:html
        [:body
-        [:h1 {:class (css {:color "blue" :text-align "center"})} "Kichou Backend"]]]
+        [:h1 {:class (css {:color "blue" :text-align "center"})} "Hi Kichou"]]]
       (h/html)
       (str)))
+
 
 (defn handler [_]
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    (home)})
 
-;;; Remove the dev-handler once dev is done
+(defn stop-server! []
+  (when @server
+    (.stop @server)
+    (reset! server nil)
+    (println "Server shut down")))
+
+(defn start-server! [port]
+  (when @server
+    (stop-server!))
+  (reset! server (jetty/run-jetty handler {:port port, :join? false}))
+  (println "Server started at port: " port))
 
 (defn -main [& args]
-  (jetty/run-jetty handler {:port  3210,
-                            :join? false}))
+    (start-server! 3000))
 
 (comment (-main))
+
+(comment (stop-server!))
